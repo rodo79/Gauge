e2880737e2a9b375c0010da3005f9c88e82c2f3f
rodolphe <rodolphe.duperray@free.fr>
2014-01-30 15:01:36 +0100

    update files

--- designer.js
+++ designer.js
@@ -1,6 +1,4 @@
-;(function() {
-
-    var widget = Widget.Gauge.inherit(WAF.require('waf-behavior/studio'));
+;(function(widget) {
 
     widget.setDescription('Gauge');
     
@@ -8,55 +6,12 @@
     widget.setWidth('180');
     widget.setHeight('180');
 
-	/*Properties*/
-    widget.addAttributes([{
-        'name': 'data-binding',
-        'description': 'Source',
-        'typeValue': 'datasource'
-    },{
-        'name': 'data-displayname',
-        'description': 'Name',
-        'typeValue': 'string',
-        'defaultValue': 'Gauge' 
-    },{
-        'name': 'data-minvalue',
-        'description': 'Min value',
-        'typeValue': 'number',
-        'defaultValue': 0 
-    },{
-        'name': 'data-maxvalue',
-        'description': 'Max value',
-        'typeValue': 'number',
-        'defaultValue': 100
-    }]);
-
 
     widget.on('resize', function(event) {
 
-		var width = event.size.width,
-			height = event.size.height,
-			size;
-
-		size = width;		
-
-		if (width > height) {
-			size = height;
-		}	
-
-		this.options.size  = size + 20;
+		this.config.size = this.fixSize();
+		this.render();
+		
     });
 
-    widget.on('display', function(attributes) {
-
-    	var that = this;
-
-    	this.options.displayname 	= attributes["data-displayname"];
-    	this.options.maxvalue  		= attributes["data-maxvalue"];
-    	this.options.minvalue  		= attributes["data-minvalue"];
-
-    	window.setTimeout(function(){
-    		that.init();
-    	}, 0);
-    	
-    });
-})();
+});
--- widget.js
+++ widget.js
@@ -1,78 +1,112 @@
-(function() {
+WAF.define('Gauge', function() {
 
     var Widget = WAF.require('waf-core/widget'),
     	widget = Widget.create('Gauge');
 
-   /**
-    * Creates the widget, automatically called by the framework.
-    */
     widget.prototype.init = function () {
-
-    	var myGauges,
-    		that = this,
-    		label,
-    		size, 
-    		min,
-    		max,
-    		width,
-    		height,
-    		$htmlElement;
-
-    	//manage gauge size depending on widget size	
-    	$htmlElement = $("#"+this.id);
-    	width = parseInt($htmlElement.css("width"), 10);
-    	height = parseInt($htmlElement.css("height"), 10);
-    	size = width;			
-		if (width > height) {
-	 		size = height;
-	 	}
-    	size = size + 20;	
-
-    	//set options values
-		label 	= this.options.displayname || "Gauge";
-    	min 	= parseInt(this.options.minvalue, 10) || 0;
-    	max 	= parseInt(this.options.maxvalue, 10) || 100;
-  
-        $(this.node).empty();
-        
-        this.containerID = 'gaugeContainer'+this.id;
-
-        myGauges  = document.createElement('span');
-        myGauges.id = this.containerID;
-
-        $(this.node).append(myGauges);
+    	 
+    	//creating an unique id for the container 
+    	this.containerID ='gaugeContainer'+this.id
+		//adding a span node to my widget
+        $(this.node).html('<span id="'+this.containerID+'"></span>');
 			
-		function createGauge() {
-			var config = {
-				size: size,
-				label: label,
-				min: min,
-				max: max,
-				minorTicks: 5
-			}
-			
-			var range = config.max - config.min;
-			config.yellowZones = [{ from: config.min + range*0.75, to: config.min + range*0.9 }];
-			config.redZones = [{ from: config.min + range*0.9, to: config.max }];
-			
-			that.gauges = new Gauge(that.containerID, config);
-			that.gauges.render();
+		// the gauge config values	
+		this.config = {
+			size: this.fixSize(),
+			label: this.displayname(), 
+			min: this.minvalue(),
+			max: this.maxvalue(),
+			minorTicks: 5
 		}
+
+		//defining the red and yellow zones for the gauge
+        this.getZones();
+			
+		//creating the gauge attribute 
+		this.gauge = new Gauge(this.containerID, this.config);
+
+		//rendering the widget
+		this.render();
 		
-		createGauge();
-
-    };   
-
-    /**
-	 * allow to set the value of the gauge
-	 * @class 
-	 * @method value 
-	 * @param {String} myValue Value of the data-binding property 
-	 */
-   	widget.prototype.value = function(myValue) { 
-    	this.gauges.redraw(myValue); 
-    };	
-
-    widget.makeBindableProperty(widget.prototype.value);
-
-})();
+		//Defining the onChange method for the relevant attributes
+		this.value.onChange( function() { 
+    		this.gauge.redraw(this.value()); 
+    	});	
+    	this.displayname.onChange( function(value) {
+    		this.config.label = value; 
+    		this.render(); 
+    	});	
+    	this.minvalue.onChange( function(value) {
+    		this.config.min = value; 
+    		this.render(); 
+    	});	
+    	this.maxvalue.onChange( function(value) {
+    		this.config.max = value; 
+    		this.render(); 
+    	});	
+    };  
+    
+       
+    widget.prototype.getZones = function () {   	
+		var range = this.config.max - this.config.min;
+        console.log(range);
+		this.config.yellowZones = [{ from: this.config.min + range * 0.75, to: this.config.min + range*0.9 }];
+		this.config.redZones = [{ from: this.config.min + range * 0.9, to: this.config.max }];
+	};
+     
+    
+    widget.prototype.fixSize = function () {
+    	
+    	//getting the size
+    	var width = this.width();
+    	var height = this.height();
+    	
+    	//making sure we have a square widget and multiplying by 0.9 to adjust the widget size to the gauge.js library size		
+		if (width > height) {
+	 		return height/0.9;
+	 	}
+	 	return width/0.9;
+	};
+    
+   
+    widget.prototype.render = function () {
+   		//emptying the node
+    	$('>span', this.node).empty();
+    	//defining the red and yellow zones for the gauge
+		this.getZones();
+		//creating the gauge attribute
+		//this.gauge = new Gauge(this.containerID, this.config);
+    	//reconfiguring the gauge library with options
+    	this.gauge.configure(this.config);
+    	//rendering the gauge library
+    	this.gauge.render();
+    	//redrawing the gauge library with 'this'
+    	this.gauge.redraw(this.value()); 
+    };
+ 
+ 	//Adding a bindable property
+   	widget.addProperty('value', {
+   		'type' : 'number', 
+   		'defaultValue' : 0 
+   	});
+ 
+ 	//Adding non bindable properties
+    widget.addProperty('displayname', { 
+    	'defaultValue' : 'Gauge', 
+    	'bindable' : false
+    });
+    
+     widget.addProperty('minvalue', { 
+        'type' : 'number',
+    	'defaultValue' : 0, 
+    	'bindable' : false
+    });
+ 
+    widget.addProperty('maxvalue', { 
+        'type' : 'number',
+    	'defaultValue' : 100, 
+    	'bindable' : false
+    });   	   	
+ 
+	return widget;
+});
